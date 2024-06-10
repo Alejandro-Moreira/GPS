2d1056f38db9b0848d28615fca452f8479af7d52
Alejandro-Moreira <moreiramikel11@gmail.com>
2024-06-10 02:04:44 -0500

    Localizacion en Firestore

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+// geolocation and native-geocoder
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+  providers: [
+    Geolocation,
+    NativeGeocoder,
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy
+    }
+  ],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule {}
