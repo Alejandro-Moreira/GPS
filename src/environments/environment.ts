622ed322742d4a7aeded203895cd4ee01200037f
Alejandro Moreira <117743484+Alejandro-Moreira@users.noreply.github.com>
2024-06-10 02:16:41 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyDLFNRiDElP27QW0EGCC6811DqLzlQGvTY",
-    authDomain: "gpsfirebase-750df.firebaseapp.com",
-    projectId: "gpsfirebase-750df",
-    storageBucket: "gpsfirebase-750df.appspot.com",
-    messagingSenderId: "920172188026",
-    appId: "1:920172188026:web:a88cd04bd54619a79d86c3"
-  }
 };
 
 /*
