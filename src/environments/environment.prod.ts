db2a208088b479f463e9cba27cf76ffd1d4b63e7
Alejandro Moreira <117743484+Alejandro-Moreira@users.noreply.github.com>
2024-06-10 02:16:18 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyDLFNRiDElP27QW0EGCC6811DqLzlQGvTY",
-    authDomain: "gpsfirebase-750df.firebaseapp.com",
-    projectId: "gpsfirebase-750df",
-    storageBucket: "gpsfirebase-750df.appspot.com",
-    messagingSenderId: "920172188026",
-    appId: "1:920172188026:web:a88cd04bd54619a79d86c3"
-  }
+  production: true
 };
